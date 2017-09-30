32cbd985cac2016b7b0122f4550cc3499630d9a6
papapapapapapapa <r3386588@gmail.com>
2017-09-30 14:41:12 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x;
+        x=in.nextInt();
+        System.out.printf("%.1f\n",x*1.6);
+        System.out.println();
+        double y;
+        y=in.nextDouble();
+        System.out.println(y*1.6);
+
+    }
+}
